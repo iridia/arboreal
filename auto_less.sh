987ed2bc9167d71702b3a7a46035d78c2fc3416b
Evadne Wu <ev@monoceroi.com>
2010-06-23 22:44:40 +0800

    adds auto_less and auto_pictorial to ease development

---
+++ auto_less.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+lessc frontend/ui/arboreal.style.less --watch --growl --timestamps --verbose
+
+
+
+
---
+++ auto_pictorial.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+pictorial.rb --confirm-overwrite --from-directory "design" --to-directory "frontend/ui" --rename-from "okogreen.scaffold_(.+).png" --rename-to "\\1.png"
+
+
+
+
