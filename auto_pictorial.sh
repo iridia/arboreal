f49902c9f60dd71628cf0e8666a2254ea5a0a6a4
Evadne Wu <ev@monoceroi.com>
2010-06-27 14:28:14 +0800

    updates auto_pictorial so it does not pick up png.tmp$$ files

--- auto_pictorial.sh
+++ auto_pictorial.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-pictorial.rb --confirm-overwrite --from-directory "design" --to-directory "frontend/ui" --rename-from "okogreen.scaffold_(.+).png" --rename-to "\\1.png"
+pictorial.rb --overwrite-existing-file --from-directory "design" --to-directory "frontend/ui" --rename-from "okogreen.scaffold_(.+).png$" --rename-to "\\1.png"
 
 
 
