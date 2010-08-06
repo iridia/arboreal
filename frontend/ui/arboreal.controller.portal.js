iridia/arboreal

commit 83fe50efe596d63a007d1616d57bfe1fad3b1b0a

adds .eq(0) guards in the portal’s bindings so the script is safer

frontend/ui/arboreal.controller.portal.js
@@ -25,14 +25,14 @@ arboreal.controller.portal = new JS.Singleton(arboreal.controller.archetype, {
 	
 		this.bindings = {
 	
-			twitterStreamHolder: $("article section.twitterStream"),
+			twitterStreamHolder: $("article section.twitterStream").eq(0),
 	
-			calendarDateTitle: $("aside .calendar h2:eq(0)"),
-			calendarDateHolder: $("aside .calendar .dates"),
-			calendarDetailsHolder: $("aside .calendar .details"),
-			calendarSubscriptionAnchor: $("aside .calendar .actions a:eq(0)"),
+			calendarDateTitle: $("aside .calendar h2:eq").eq(0),
+			calendarDateHolder: $("aside .calendar .dates").eq(0),
+			calendarDetailsHolder: $("aside .calendar .details").eq(0),
+			calendarSubscriptionAnchor: $("aside .calendar .actions a").eq(0),
 			
-			calendarTooltip: $("aside .calendar .tooltip:eq(0)")
+			calendarTooltip: $("aside .calendar .tooltip").eq(0),
 		
 		};
 		

commit 2134e59546dc212e043919ee04ff97927d193fdc

fixes a bug introduced by 83fe50efe596d63a007d1616d57bfe1fad3b1b0a where a stray eq() is not removed

frontend/ui/arboreal.controller.portal.js
@@ -27,7 +27,7 @@ arboreal.controller.portal = new JS.Singleton(arboreal.controller.archetype, {
 	
 			twitterStreamHolder: $("article section.twitterStream").eq(0),
 	
-			calendarDateTitle: $("aside .calendar h2:eq").eq(0),
+			calendarDateTitle: $("aside .calendar h2").eq(0),
 			calendarDateHolder: $("aside .calendar .dates").eq(0),
 			calendarDetailsHolder: $("aside .calendar .details").eq(0),
 			calendarSubscriptionAnchor: $("aside .calendar .actions a").eq(0),